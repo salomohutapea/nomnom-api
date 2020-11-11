aad35ba16447df47900e652fa7977f117b780e02
salomohutapea <hutapeasalomo@gmail.com>
2020-11-11 18:59:29 +0700

    Added get menu data

--- src/app.js
+++ src/app.js
@@ -8,19 +8,13 @@ const Menu = require('../src/models/menumodel')
 // Connect to MongoDB
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
-// Set ejs file as default view
-app.set('view engine', 'ejs')
-
-// Define static folder, such as CSS and images
-app.use(express.static(__dirname + '/static'));
-
 app.get('/', (req, res) => {
     res.send('Nom Nom API')
 })
 
 // Request any data from database collection
-app.get('/:data', (req, res) => {
-    const data = mongoose.model(req.params.data)
+app.get('/data', (req, res) => {
+    const data = mongoose.model(req.query.q)
 
     // Find menus in database and pass the data to view
     data.find({}, function (err, data) {
@@ -32,11 +26,18 @@ app.post('/newmenu', async (req, res) => {
 
     // Add new menu to database
 
-    const newMenu = new Menu(req.body)
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        res.send('Harap masukkan menu')
+        res.status(403)
+    } else {
+        const menu = new Menu(req.body)
 
-    try {
-        newMenu = await menu.save()
-    } catch {
+        try {
+            menu = await menu.save()
+        } catch {
+            res.status(405)
+            res.send('Menu gagal ditambahkan')
+        }
 
     }
 })
